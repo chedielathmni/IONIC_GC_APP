613eb1f28b3520a296c702b366ce975e0ece1d14
chedielathmni <chedielathmni@gmail.com>
2020-06-01 14:49:02 +0100

    updated tab3: added changePassword Form

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -8,8 +9,50 @@ import { AuthService } from '../services/auth.service';
 })
 export class Tab3Page {
 
-  constructor(
-    private auth: AuthService
-  ) {}
 
+  changePasswordForm: FormGroup;
+
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder
+  ) {
+
+    this.changePasswordForm = this.formBuilder.group({
+      oldPassword: [
+        '',
+        Validators.compose([
+          Validators.maxLength(32),
+          Validators.minLength(6)
+        ])
+      ],
+
+      newPassword: [
+        '',
+        Validators.compose([
+          Validators.maxLength(32),
+          Validators.minLength(6)
+        ])
+      ],
+
+      confirmNewPassword: [
+        '',
+        Validators.compose([
+          Validators.maxLength(32),
+          Validators.minLength(6)
+        ])
+      ]
+    }, { validator: Tab3Page.passwordsMatch })
+  }
+
+
+  static passwordsMatch(cg: FormGroup): { [err: string]: any } {
+    let pwd1 = cg.get('newPassword');
+    let pwd2 = cg.get('confirmNewPassword');
+    let rv: { [error: string]: any } = {};
+    if ((pwd1.touched || pwd2.touched) && pwd1.value !== pwd2.value) {
+      rv['passwordMismatch'] = true;
+      rv['error'] = 'Mismatching passwords'
+    }
+    return rv;
+  }
 }
